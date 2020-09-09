7cc974498147ec4b91102b89261ee8aaf0b079ac
Santiago <santiagosalas.6532@gmail.com>
2020-09-08 22:26:08 -0500

    ShareOnGitHub SpringAPI JAVA Market Initial commit

---
+++ src/main/java/com/marketSantiagoSpring/MarketSantiagoApplication.java
@@ -0,0 +1,13 @@
+package com.marketSantiagoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSantiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSantiagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketSantiagoSpring/MarketSantiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketSantiagoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSantiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
