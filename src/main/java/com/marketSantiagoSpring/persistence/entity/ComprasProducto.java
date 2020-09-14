d1933a408a613fd8d0cb709534f8dbb49a675669
Santiago <santiagosalas.6532@gmail.com>
2020-09-14 00:41:42 -0500

    Entity para el proyecto, definiendo atributos de clases

---
+++ src/main/java/com/marketSantiagoSpring/persistence/entity/ComprasProducto.java
@@ -0,0 +1,58 @@
+package com.marketSantiagoSpring.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_producto")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+
+    private Integer cantidad;
+
+
+    private Double total;
+
+
+    private Boolean estado;
+
+    //Getter And Setters
+
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
