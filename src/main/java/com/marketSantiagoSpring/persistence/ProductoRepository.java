974d466fff413a74010310947524fa2213279b1c
Santiago <santiagosalas.6532@gmail.com>
2020-09-21 21:21:22 -0500

    QueryMethods

--- src/main/java/com/marketSantiagoSpring/persistence/ProductoRepository.java
+++ src/main/java/com/marketSantiagoSpring/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.marketSantiagoSpring.persistence.crud.ProductoCrudRepository;
 import com.marketSantiagoSpring.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -11,5 +12,15 @@ public class ProductoRepository {
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    //Obtencion de todos los productos de una categoria y ordenados de forma ascendente
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
