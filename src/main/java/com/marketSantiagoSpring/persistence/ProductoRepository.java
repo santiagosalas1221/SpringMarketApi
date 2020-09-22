6f359f7be48388ad49861af3d31b4fb470311573
Santiago <santiagosalas.6532@gmail.com>
2020-09-21 21:46:21 -0500

    Data Mapper

--- src/main/java/com/marketSantiagoSpring/persistence/ProductoRepository.java
+++ src/main/java/com/marketSantiagoSpring/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.marketSantiagoSpring.persistence;
 
 import com.marketSantiagoSpring.persistence.crud.ProductoCrudRepository;
 import com.marketSantiagoSpring.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -23,4 +25,22 @@ public class ProductoRepository {
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
+
+    //Obtener producto por ID
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+
+    //Guardar Producto
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+
+    //Eliminar producto
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 }
